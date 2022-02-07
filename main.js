f90a136dad70f16a8b97864b1020251410db0a5d
MaissamB <maiss.ben@gmail.com>
2022-02-07 12:22:44 +0100

    exo 2 start

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const helloWorld = React.createElement('h1', {}, 'Hello world!');
 // mauvaise pratique
 
 
-//ReactDOM.render(helloWorld, document.body);
+ReactDOM.render(helloWorld, document.body);
 
 // Meilleure pratique mais pas encore LA meilleure
 
