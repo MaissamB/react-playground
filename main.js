5bd578bdb9a4f47d65413b730ec85d344f0fe15d
MaissamB <maiss.ben@gmail.com>
2022-02-08 13:49:01 +0100

    reflection exo5

--- main.js
+++ main.js
@@ -3,35 +3,68 @@ const lastName = 'formateur';
 
 function FirstName(props) {
 
-    /*
+    
     // Solution avec bonus
     const formatFirstName = (firstName) => {
         return firstName[0].toUpperCase() + firstName.substr(1);
     }
 
     return <span>{formatFirstName(props.text)}</span>
-    */
+    
 
     // Solution sans bonus
-    return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
+    //return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
 }
 
 function LastName(props) {
 
-    /*
+    
     // Solution avec bonus
     const formatLastName = (lastName) => {
         return lastName.toUpperCase();
     }
 
     return <span>{formatLastName(props.text)}</span>
-    */
+    
 
     // Solution sans bonus
-    return <span className="red-text">{props.text.toUpperCase()}</span>
+    //return <span className="red-text">{props.text.toUpperCase()}</span>
 
 }
 
-const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
+class Clock extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {date: new Date()}
+    }
+    render(){
+        return (
+            <div>
+            <h1>Bonjour, monde !</h1>
+            <h2>Il est {this.props.date.toLocaleTimeString('fr-FR')}.</h2>
+            </div>
+        );
+    }
+}
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+/*function Clock(props) {
+    return (
+        <div>
+        <h1>Bonjour, monde !</h1>
+        <h2>Il est {props.date.toLocaleTimeString('fr-FR')}.</h2>
+        </div>
+    );
+}*/
+
+function tick() {
+    ReactDOM.render(
+        <h1><Clock date={new Date()} /></h1>,
+        document.querySelector('#app')
+    );
+}
+setInterval(tick, 1000);    
+
+
+//const helloWorld = <h1><Clock date={new Date()} /></h1>;
+
+//ReactDOM.render(helloWorld, document.querySelector('#app'));
